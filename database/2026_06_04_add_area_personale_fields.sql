341bc42e056bf2b39fc686a2d295d34104ce18e4
lorenzodifee123 <lorenzodifee@gmail.com>
2026-06-18 19:16:04 +0200

    Mostra target nella pagina recensione

--- database/2026_06_04_add_area_personale_fields.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE utenti
-  ADD COLUMN localita VARCHAR(150) NULL AFTER foto_profilo,
-  ADD COLUMN biografia TEXT NULL AFTER localita;
--- database/2026_06_04_add_foto_profilo.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE utenti
-  ADD COLUMN foto_profilo VARCHAR(500) NULL AFTER telefono;
